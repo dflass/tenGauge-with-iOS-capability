8edec0a02785f2bcf92711688ba35fc4ac7b5f9f
Derek Duncan <dgduncan@Dereks-MacBook-Pro-2.local>
2016-01-25 15:17:06 -0600

    minor bug fixes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
-//#include <QtWidgets>
 
-
-
+#if defined(Q_OS_IOS)
 extern "C" int qtmn(int argc, char *argv[])
+#else
+int main(int argc, char *argv[])
+#endif
 {
     QApplication a(argc, argv);
+
+    //MainWindow Initialization
     MainWindow w;
     w.setWindowTitle("Dr. Jackson's Boat");
-
     w.show();
+    //**************************
 
     return a.exec();
-
-
 }
+
